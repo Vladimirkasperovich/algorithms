0320e6e843cdc7efa997e08f1deb8a63716a64fa
vovak <vova.kasper.boy@gmail.com>
2025-08-07 13:36:28 +0300

    feat: deepClone

--- algorithms.js
+++ algorithms.js
@@ -1,1 +1,23 @@
 'use strict';
+function deepClone(obj) {
+  if (obj === null) return obj;
+  if (obj && typeof obj === 'object' && Array.isArray(obj)) {
+    return obj.map(deepClone);
+  }
+
+  const result = {};
+  for (const key in obj) {
+    if (typeof obj[key] === 'object') {
+      result[key] = deepClone(obj[key]);
+    } else {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+}
+
+// const source = { a: 1, b: { c: 2 } };
+// const copy = deepClone(source);
+// console.log(copy); // { a: 1, b: { c: 2 } }
+// console.log(copy === source); // false
+// console.log(copy.b === source.b); // false
