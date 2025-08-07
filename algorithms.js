6c872fed391aebf35e3e678c6f516c9ac9cf52b1
vovak <vova.kasper.boy@gmail.com>
2025-08-07 13:47:30 +0300

    feat: flatten

--- algorithms.js
+++ algorithms.js
@@ -36,3 +36,19 @@ function debounce(fn, delay) {
 // console.log(debounced());
 // console.log(debounced());
 // console.log(debounced());
+
+function flatten(arr) {
+  const result = [];
+
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      result.push(...flatten(item));
+    } else {
+      result.push(item);
+    }
+  });
+
+  return result;
+}
+
+// console.log(flatten([1, [2, [3, 4]], 5])); // [1, 2, 3, 4, 5]
