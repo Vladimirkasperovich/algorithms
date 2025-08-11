cf7552cd2b1300fcc32ae472e83e8fd060501665
vovak <vova.kasper.boy@gmail.com>
2025-08-11 16:19:04 +0300

    feat: hasUniqueChars

--- algorithms.js
+++ algorithms.js
@@ -7,3 +7,17 @@ function isAnagram(str1, str2) {
 // console.log(isAnagram('listen', 'silent')); // true
 // console.log(isAnagram('hello', 'world')); // false
 // console.log(isAnagram('evil', 'vile')); // true
+
+function hasUniqueChars(str) {
+  if (!str.length) return true;
+  const seen = new Map();
+  for (const sElement of str.toLowerCase()) {
+    seen.set(sElement, (seen.get(sElement) || 0) + 1);
+  }
+
+  return [...seen.values()].every((value) => value === 1);
+}
+
+// console.log(hasUniqueChars('abcdef')); // true
+// console.log(hasUniqueChars('aabcde')); // false
+// console.log(hasUniqueChars('')); // true
