2b77f078d3fe7d2adf5ee5e49295660ff3531016
vovak <vova.kasper.boy@gmail.com>
2025-08-18 16:03:53 +0300

    feat: TimeLimitedCache

--- algorithms.js
+++ algorithms.js
@@ -275,3 +275,47 @@ const memoize = (fn) => {
 // console.log(memoizedFn(2, 3)); // 5
 // console.log(memoizedFn(2, 3)); // 5
 // console.log(callCount); // 1
+
+function TimeLimitedCache() {
+  this.cache = new Map();
+}
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  const hasKey = this.cache.has(key);
+  if (hasKey) {
+    clearTimeout(this.cache.get(key).timeoutID);
+  }
+
+  this.cache.set(key, {
+    value,
+    timeoutID: setTimeout(() => this.cache.delete(key), duration),
+  });
+
+  return hasKey;
+};
+
+/**
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function (key) {
+  return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+  return this.cache.size;
+};
+
+// const timeLimitedCache = new TimeLimitedCache();
+// console.log(timeLimitedCache.set(1, 42, 1000)); // false
+// console.log(timeLimitedCache.get(1)); // 42
+// console.log(timeLimitedCache.count()); // 1
