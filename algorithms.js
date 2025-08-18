65de40d96e8078308e021e392b7d837b7c2f06b7
vovak <vova.kasper.boy@gmail.com>
2025-08-18 15:07:38 +0300

    feat: Array.prototype.last

--- algorithms.js
+++ algorithms.js
@@ -223,3 +223,14 @@ const groupActiveUsers = (users) => {
 //   { name: 'Jon', isActive: true, age: 45 },
 // ];
 // console.log(groupActiveUsers(arr));
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+  if (this.length === 0) return -1;
+  return this[this.length - 1];
+};
+
+// const arr = [1, 2, 3];
+// console.log(arr.last());
