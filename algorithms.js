299e6350a2ececa9644855d3f7cd1872f6fc1549
vovak <vova.kasper.boy@gmail.com>
2025-08-18 11:04:10 +0300

    feat: isValid

--- algorithms.js
+++ algorithms.js
@@ -22,3 +22,29 @@ const twoSum = (nums, target) => {
 // console.log(twoSum([2, 7, 11, 15], 9)); //[0,1]
 // console.log(twoSum([3, 2, 4], 6)); //[1,2]
 // console.log(twoSum([3, 3], 6)); //[0,1]
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = (s) => {
+  const bracketsMap = { '(': ')', '{': '}', '[': ']' };
+  const stack = [];
+  for (const bracket of s) {
+    if (bracketsMap[bracket]) {
+      stack.push(bracket);
+    } else {
+      const lastRemovedBracket = stack.pop();
+      if (bracketsMap[lastRemovedBracket] !== bracket) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+// console.log(isValid('()')); //true
+// console.log(isValid('()[]{}')); //true
+// console.log(isValid('(]')); //false
+// console.log(isValid('([])')); //true
+// console.log(isValid('([)]')); //false
