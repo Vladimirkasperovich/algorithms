6f25f3b13caf3c053419cd2b09637d379f0e25d6
vovak <vova.kasper.boy@gmail.com>
2025-08-26 10:20:12 +0300

    feat: sortBy

--- algorithms.js
+++ algorithms.js
@@ -778,3 +778,28 @@ Array.prototype.groupBy = function (fn) {
 //     return item.id;
 //   }),
 // ); //{"1":[{"id":"1"},{"id":"1"}],"2":[{"id":"2"}]}
+
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+const sortBy = (arr, fn) => {
+  return arr.sort((a, b) => {
+    const A = fn(a);
+    const B = fn(b);
+    return A - B;
+  });
+};
+
+// console.log(sortBy([5, 4, 1, 2, 3], (x) => x)); //[1, 2, 3, 4, 5]
+// console.log(sortBy([{ x: 1 }, { x: 0 }, { x: -1 }], (d) => d.x)); //[{"x": -1}, {"x": 0}, {"x": 1}]
+// console.log(sortBy(
+//     [
+//       [3, 4],
+//       [5, 2],
+//       [10, 1],
+//     ],
+//     (x) => x[1],
+//   ),
+// ); //[[10, 1], [5, 2], [3, 4]]
