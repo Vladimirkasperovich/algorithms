d31a2bf41717a11de1469928df06c60661cd51c5
vovak <vova.kasper.boy@gmail.com>
2025-08-26 14:47:43 +0300

    feat: kidsWithCandies

--- algorithms.js
+++ algorithms.js
@@ -973,3 +973,20 @@ const reverseWords = (s) => {
 // console.log(reverseWords('the sky is blue')); //"blue is sky the"
 // console.log(reverseWords('  hello world  ')); //"world hello"
 // console.log(reverseWords('a good   example')); //"example good a"
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+const kidsWithCandies = (candies, extraCandies) => {
+  const max = Math.max(...candies);
+  const result = [];
+  for (let i = 0; i < candies.length; i++) {
+    result.push(candies[i] + extraCandies >= max);
+  }
+  return result;
+};
+// console.log(kidsWithCandies([2, 3, 5, 1, 3], 3)); //[true,true,true,false,true]
+// console.log(kidsWithCandies([4, 2, 1, 1, 2], 1)); //[true,false,false,false,false]
+// console.log(kidsWithCandies([12, 1, 12], 10)); //[true,false,true]
