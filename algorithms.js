7d1be7e2c9c39e983c1d92cee9f5db53cb90ed73
vovak <vova.kasper.boy@gmail.com>
2025-08-29 15:45:29 +0300

    feat: splitIntoChunks

--- algorithms.js
+++ algorithms.js
@@ -1024,3 +1024,21 @@ const shuffleArray = (arr) => {
   return [...arr].sort(() => Math.random() - 0.5);
 };
 // console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7]));
+
+// Написать функцию чанк, которая принимает массив и какое-то
+// кол-во элементов должно быть в чанке.
+// Далее функция возвращает массив с массивами, как в комментах
+
+function splitIntoChunks(arr, num) {
+  // Ваш код здесь
+  const result = [];
+  for (let i = 0; i < arr.length; i += num) {
+    const chunk = arr.slice(i, i + num);
+    result.push(chunk);
+  }
+  return result;
+}
+
+// console.log(splitIntoChunks([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)); // [[1,2,3],[4,5,6],[7,8,9]]
+// console.log(splitIntoChunks([1, 2, 3, 4, 5, 6, 7, 8, 9], 4)); // [[1,2,3,4],[5,6,7,8],[9]]
+// console.log(splitIntoChunks([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)); // [[1,2],[3,4],[5,6],[7,8],[9]]
