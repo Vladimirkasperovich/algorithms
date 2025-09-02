188ec16d49ada5c8eb6c9367c093ad9f191ec4c7
vovak <vova.kasper.boy@gmail.com>
2025-09-02 12:41:57 +0300

    feat: sortByField

--- algorithms.js
+++ algorithms.js
@@ -1479,3 +1479,27 @@ function optionalChaining(obj, path) {
 //     'a.b.c.d',
 //   ),
 // ); // Ответ = Привет
+
+// Написать функцию, которая принимает массив объектов и название поля объекта и возвращает массив объектов, отсортированных по переданному полю
+
+const data = [
+  { id: 1, age: 20, name: 'Иван', country: 'Russia', registred: true },
+  { id: 2, age: 30, name: 'Дима', country: 'Usa', registred: true },
+  { id: 3, age: 25, name: 'Леха', country: 'Russia', registred: false },
+  { id: 4, age: 20, name: 'Леха', country: 'Usa', registred: false },
+  { id: 5, age: 30, name: 'Иван', country: 'Russia', registred: true },
+  { id: 6, age: 50, name: 'Леха', country: 'Russia', registred: true },
+  { id: 7, age: 20, name: 'Дима', country: 'Usa', registred: false },
+];
+
+const sortByField = (array, param) => {
+  // Ваш код здесь
+  return array.sort((a, b) => {
+    const valueA = a[param];
+    const valueB = b[param];
+    return typeof valueA === 'string'
+      ? valueA.localeCompare(valueB)
+      : valueA - valueB;
+  });
+};
+// console.log(sortByField(data, 'country'));
