199d64d8d02f54d257cf56f585f8d4f336bfad73
vovak <vova.kasper.boy@gmail.com>
2025-09-02 13:07:57 +0300

    feat: includes

--- algorithms.js
+++ algorithms.js
@@ -1503,3 +1503,44 @@ const sortByField = (array, param) => {
   });
 };
 // console.log(sortByField(data, 'country'));
+
+// Напиши функцию includes(value) для объекта, которая принимает value и возращает true,
+// если есть поле value с этим значением и false, если нет. Объект следующий:
+
+const tree = {
+  value: 1,
+  children: [
+    {
+      value: 2,
+      children: [{ value: 4 }, { value: 5 }],
+    },
+    {
+      value: 3,
+      children: [{ value: 6 }, { value: 7 }],
+    },
+  ],
+};
+
+function includes(value, tree) {
+  // Ваш код здесь
+  if (tree.value === value) return true;
+
+  if (tree.children && Array.isArray(tree.children)) {
+    for (const item of tree.children) {
+      if (includes(value, item)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// console.log(includes(1, tree));
+// console.log(includes(11, tree)); // false
+// console.log(includes(2, tree));
+// console.log(includes(3, tree));
+// console.log(includes(4, tree));
+// console.log(includes(5, tree));
+// console.log(includes(6, tree));
+// console.log(includes(7, tree));
+// console.log(includes(77, tree)); // false
