d951a96fd2da48fa8c87df6261e972b2db990328
vovak <vova.kasper.boy@gmail.com>
2025-09-12 17:22:36 +0300

    feat: reverseNumber

--- algorithms.js
+++ algorithms.js
@@ -10,9 +10,19 @@ const sumRange = (from, to) => {
 // console.log(sumRange(1, 5)); // 15
 // console.log(sumRange(0, 10)); // 55
 // console.log(sumRange(-3, 3)); // 0
-const reverseNumber = (num) => {};
+const reverseNumber = (num) => {
+  const str = num.toString();
+  if (!str.startsWith('-')) {
+    return +str.split('').reverse().join('');
+  }
+  let result = '';
+  for (let i = str.length - 1; i >= 1; i--) {
+    result += str[i];
+  }
+  return Number(`-${result}`);
+};
 
-console.log(reverseNumber(123)); // 321
-console.log(reverseNumber(-456)); // -654
-console.log(reverseNumber(1000)); // 1
-console.log(reverseNumber(0)); // 0
+// console.log(reverseNumber(123)); // 321
+// console.log(reverseNumber(-456)); // -654
+// console.log(reverseNumber(1000)); // 1
+// console.log(reverseNumber(0)); // 0
