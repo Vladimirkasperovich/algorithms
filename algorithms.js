429638973f07f44a82eccc4a183c56bca7c73af8
vovak <vova.kasper.boy@gmail.com>
2025-09-15 11:15:47 +0300

    feat: fetchRetryer

--- algorithms.js
+++ algorithms.js
@@ -53,3 +53,35 @@ const secondLargest = (arr) => {
 // console.log(secondLargest([10, 20, 4, 45, 99])); // 45
 // console.log(secondLargest([5, 5, 5])); // null
 // console.log(secondLargest([1])); // null
+
+const fetchRetryer = (url, count = 5) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((data) => {
+        if (data.status === 200) {
+          resolve(data);
+        } else {
+          throw new Error('something went wrong');
+        }
+      })
+      .catch(() => {
+        if (count > 0) {
+          fetchRetryer(url, count - 1)
+            .then(resolve)
+            .catch(reject);
+        } else {
+          reject('calls limit');
+        }
+      });
+  });
+};
+// 1. Успешный запрос с первого раза
+// fetchRetryer('https://good-api.com/data')
+//   .then((data) => console.log('Успех:', data))
+//   .catch((err) => console.log('Ошибка:', err.message));
+//
+// // 2. Успешный запрос после 2 неудач
+// // (предположим, что сервер первые 2 раза не отвечает, а на 3-й отвечает)
+// fetchRetryer('https://unstable-api.com/data')
+//   .then((data) => console.log('Успех после повторов:', data))
+//   .catch((err) => console.log('Ошибка:', err.message));
