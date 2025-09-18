0ebb8c76cb0d59cf4ae63a838634be8f73cb4eb0
vovak <vova.kasper.boy@gmail.com>
2025-09-18 16:38:39 +0300

    feat: findMaxSalary

--- algorithms.js
+++ algorithms.js
@@ -539,3 +539,41 @@ function sortOperations(operations) {
 }
 
 // console.log(sortOperations(operations));
+// Необходимо сделать выборки.
+// 1. Список имен, отсортированный по размеру зарплаты
+// 2. Размер максимальной зарплаты
+// 3. Статистика по каждому отделу: сумма затрат, количество сотрудников, средняя з/п
+
+const findMaxSalaryArray = [
+  { name: 'Вася', salary: 10000, department: 'Frontend' },
+  { name: 'Петя', salary: 12000, department: 'Backend' },
+  { name: 'Дима', salary: 10500, department: 'Frontend' },
+  { name: 'Оля', salary: 15000, department: 'Backend' },
+  { name: 'Саша', salary: 8000, department: 'Frontend' },
+  { name: 'Олег', salary: 9000, department: 'Testing' },
+];
+
+const findMaxSalary = (data) => {
+  const sortedSalary = [...data].sort((a, b) => a.salary - b.salary);
+  const sortedNames = sortedSalary.map((item) => item.name);
+  const maxSalary = sortedSalary[0].salary;
+
+  const stats = {};
+  sortedSalary.forEach(({ department, salary }) => {
+    if (!stats[department]) {
+      stats[department] = { totalSalary: 0, count: 0 };
+    }
+    stats[department].totalSalary += salary;
+    stats[department].count += 1;
+  });
+  for (const dept in stats) {
+    stats[dept].averageSalary = stats[dept].totalSalary / stats[dept].count;
+  }
+
+  return {
+    sortedNames,
+    maxSalary,
+    stats,
+  };
+};
+// console.log(findMaxSalary(findMaxSalaryArray));
