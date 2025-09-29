b2d29b03ed416454883f0808ce8937161e0eca86
vovak <vova.kasper.boy@gmail.com>
2025-09-29 10:53:18 +0300

    feat: getSortedArray

--- algorithms.js
+++ algorithms.js
@@ -10,3 +10,22 @@ const findMaxNumber = (array) => {
   return max;
 };
 // console.log(findMaxNumber([1, 2, 3, 4, 5, 6]));
+
+// Напишите функцию сортировки массива
+// (исходный массив должен остаться без изменений)
+const getSortedArray = (arr) => {
+  if (arr.length < 1) return [...arr];
+  const left = [];
+  const right = [];
+  const pivot = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    const num = arr[i];
+    if (num < pivot) {
+      left.push(num);
+    } else {
+      right.push(num);
+    }
+  }
+  return [...getSortedArray(left), pivot, ...getSortedArray(right)];
+};
+// console.log(getSortedArray([1, 5, 2, 4, 3]));
