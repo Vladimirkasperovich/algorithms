0b31450f9c30a30d775ca3978b52cafda95a9ce5
vovak <vova.kasper.boy@gmail.com>
2025-09-29 11:33:20 +0300

    feat: shuffleArray

--- algorithms.js
+++ algorithms.js
@@ -83,3 +83,17 @@ function serializeElementFrequencies(arr) {
 }
 
 // console.log(serializeElementFrequencies([1, 2, 3, 4, 4])); // {'1:1, 2:1, 3:1, 4:2'}
+
+// Реализовать функцию, которая будет принимать на вход массив чисел и всегда возвращать
+// массив этих же чисел, но каждый раз в рандомном порялке
+
+function shuffleArray(array) {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
+// console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7]));
