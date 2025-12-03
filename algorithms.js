480f0468409d1143e228df31f6bf1c1f45eb7662
vovak <vova.kasper.boy@gmail.com>
2025-12-03 15:40:36 +0300

    feat: delayedArrayLogging

--- algorithms.js
+++ algorithms.js
@@ -60,3 +60,13 @@ function sumPositiveEvenNumbers(arr) {
 }
 
 // console.log(sumPositiveEvenNumbers([5, 0, -5, 20, 88, 17, -32])); // 108
+
+//изменить код так, что бы в итоге выводился массив i ([], [1], [1, 1]) (сейчас выводится [1, 1, 1])
+
+function delayedArrayLogging() {
+  for (let i = []; i.length < 3; i.push(1)) {
+    setTimeout(() => console.log(i), i.length * 1000);
+  }
+}
+
+// console.log(delayedArrayLogging());
