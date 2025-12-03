679183aa6ca3cb8decd828f536df916062950635
vovak <vova.kasper.boy@gmail.com>
2025-12-03 15:54:06 +0300

    feat: serializeElementFrequencies

--- algorithms.js
+++ algorithms.js
@@ -70,3 +70,19 @@ function delayedArrayLogging() {
 }
 
 // console.log(delayedArrayLogging());
+
+function serializeElementFrequencies(arr) {
+  const objectElements = arr.reduce((obj, key) => {
+    if (!obj[key]) {
+      obj[key] = 1;
+    } else {
+      obj[key] += 1;
+    }
+    return obj;
+  }, {});
+  return Object.entries(objectElements)
+    .map(([key, val]) => `${key}:${val}`)
+    .join(', ');
+}
+
+// console.log(serializeElementFrequencies([1, 2, 3, 4, 4])); // {'1:1, 2:1, 3:1, 4:2'}
