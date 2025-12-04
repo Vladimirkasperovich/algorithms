504935b63bd79d5a3c82504debabe727d591e3ff
vovak <vova.kasper.boy@gmail.com>
2025-12-04 15:13:13 +0300

    feat: getUniqDigits

--- algorithms.js
+++ algorithms.js
@@ -116,3 +116,19 @@ function splitIntoChunks(arr, num) {
 // console.log(splitIntoChunks([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)); // [[1,2,3],[4,5,6],[7,8,9]]
 // console.log(splitIntoChunks([1, 2, 3, 4, 5, 6, 7, 8, 9], 4)); // [[1,2,3,4],[5,6,7,8],[9]]
 // console.log(splitIntoChunks([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)); // [[1,2],[3,4],[5,6],[7,8],[9]]
+
+// вернуть массив уникальных чисел
+const getUniqDigits = (arr) => {
+  const numbers = arr.map(Number);
+  const numbersWithoutNan = numbers.filter((el) => !isNaN(el));
+  const seenNums = {};
+  numbersWithoutNan.forEach((num) => {
+    if (!seenNums[num]) {
+      seenNums[num] = num;
+    }
+  });
+  return [...Object.values(seenNums)].sort((a, b) => a - b);
+};
+
+// console.log(getUniqDigits(['a', 0, 4, '4', '0', 5, 'd', 7, 0, '8', 7, 10])); // output [0,1,2,3,4,5,6,7,8,9,10]`
+// console.log(getUniqDigits(['s', 1, 3, '9', 10, 3, 1, 9, 'u', 6, 5, '2'])); // output [0,1,2,3,4,5,6,7,8,9,10]`
