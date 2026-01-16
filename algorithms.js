c8b0a0b7e5f8e0012e961fe6010837ff8dd542b4
vovak <vova.kasper.boy@gmail.com>
2026-01-16 15:50:07 +0300

    feat: intersection

--- algorithms.js
+++ algorithms.js
@@ -154,3 +154,10 @@ function highestFrequency(array) {
 // console.log(highestFrequency(['a', 'b', 'c', 'c', 'd', 'e'])); // -> c
 // console.log(highestFrequency(['abc', 'def', 'abc', 'def', 'abc'])); // -> abc
 // console.log(highestFrequency(['abc', 'def'])); // -> abc
+
+const intersection = (arr1, arr2) => {
+  const set1 = new Set(arr1);
+  return [...new Set(arr2.filter((el) => set1.has(el)))];
+};
+// console.log(intersection([1, 2, 1, 2], [2, 2])); //[2]
+// console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4])); //[9, 4]
