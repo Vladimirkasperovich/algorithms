a909fbd7a9cc7e605231e23d2775347921c302ae
vovak <vova.kasper.boy@gmail.com>
2026-03-17 11:07:28 +0300

    feat: counter 1

--- algorithms.js
+++ algorithms.js
@@ -20,3 +20,11 @@ const twoSum = (nums, target) => {
 // console.log(twoSum([2, 7, 11, 15], 9)); //[0, 1]
 // console.log(twoSum([3, 2, 4], 6)); //[1, 2]
 // console.log(twoSum([3, 3], 6)); //[0, 1]
+
+// const createCounter = (n) => {
+//   return () => n++;
+// };
+// const counter = createCounter(0);
+// console.log(counter()); // 0
+// console.log(counter()); // 1
+// console.log(counter()); // 2
