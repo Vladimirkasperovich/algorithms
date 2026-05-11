1b905d6eb844000197d9b89ffe38e933943daf9e
vovak <vova.kasper.boy@gmail.com>
2026-05-11 12:41:37 +0300

    feat: firstUniqueChar

--- algorithms.js
+++ algorithms.js
@@ -1,106 +1,26 @@
 'use strict';
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+/*
+ * Задача 1
+ * Напиши функцию, которая принимает строку
+ * и возвращает первый НЕ повторяющийся символ.
+ * Если такого символа нет — вернуть null.
+ *
+ * Учитывай регистр.
  */
 
-const twoSum = (nums, target) => {
-  if (nums.length < 2) return [];
-  const map = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    const current = nums[i];
-    const diff = target - current;
-    if (map.has(diff)) return [map.get(diff), i];
-    map.set(current, i);
+function firstUniqueChar(str) {
+  const hashMap = {};
+  for (const letter of str) {
+    hashMap[letter] = (hashMap[letter] || 0) + 1;
   }
-  return [];
-};
-
-const createMemory = () => {
-  const arr = [];
-  return (val) => {
-    arr.push(val);
-    return arr;
-  };
-};
-// const remember = createMemory();
-//
-// console.log(remember(1)); // [1]
-// console.log(remember(5)); // [1, 5]
-// console.log(remember(3)); // [1, 5, 3]
-
-const createCounter = (init) => {
-  let currentCount = init;
-  return {
-    increment: () => ++currentCount,
-    decrement: () => --currentCount,
-    reset: () => {
-      currentCount = init;
-      return currentCount;
-    },
-  };
-};
-// const counter = createCounter(5);
-//
-// console.log(counter.increment()); // 6
-// console.log(counter.increment()); // 7
-// console.log(counter.decrement()); // 6
-// console.log(counter.reset()); // 5
-
-const orders = [
-  { customerId: 'A', amount: 100, status: 'completed' },
-  { customerId: 'B', amount: 200, status: 'completed' },
-  { customerId: 'A', amount: 50, status: 'completed' },
-  { customerId: 'C', amount: 300, status: 'pending' },
-  { customerId: 'B', amount: 50, status: 'pending' },
-];
-
-// функция должна вернуть массив customerId (в порядке убывания суммы всех заказов),
-// содержащий не более topN клиентов, у которых есть хотя бы один завершённый заказ
-function getTopCustomers(orders, topN) {
-  const map = {};
-
-  for (const order of orders) {
-    if (order.status !== 'completed') continue;
-    const key = order.customerId;
-
-    if (map[key]) {
-      map[key] += order.amount;
-    } else {
-      map[key] = order.amount;
-    }
+  for (const letter of str) {
+    if (hashMap[letter] === 1) return letter;
   }
-  return Object.entries(map)
-    .sort((a, b) => b[1] - a[1])
-    .slice(0, topN)
-    .map(([customerId]) => customerId);
+  return null;
 }
 
-// const result = getTopCustomers(orders, 2);
-// console.log(result);
-const tree = {
-  value: 1,
-  children: [
-    {
-      value: 2,
-      children: [{ value: 3 }],
-    },
-    {
-      value: 4,
-      children: [{ value: 5 }, { value: 6 }],
-    },
-  ],
-};
-
-function getTreeValues(tree) {
-  const result = [tree.value];
-  if (!Array.isArray(tree.children)) return result;
-  for (const node of tree.children) {
-    result.push(...getTreeValues(node));
-  }
-  return result;
-}
-
-// console.log(getTreeValues(tree)); // -> [1, 2, 3, 4, 5, 6];
+// console.log(firstUniqueChar('aabccdeff')); // 'b'
+// console.log(firstUniqueChar('aabbcc')); // null
+// console.log(firstUniqueChar('JSJavaScript')); // 'v'
+// console.log(firstUniqueChar('1122334455a')); // 'a'
