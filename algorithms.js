0c66276a084a61b4a8fb357dbeabbea039d6edac
vovak <vova.kasper.boy@gmail.com>
2026-05-11 13:15:51 +0300

    feat: isAnagram

--- algorithms.js
+++ algorithms.js
@@ -24,3 +24,34 @@ function firstUniqueChar(str) {
 // console.log(firstUniqueChar('aabbcc')); // null
 // console.log(firstUniqueChar('JSJavaScript')); // 'v'
 // console.log(firstUniqueChar('1122334455a')); // 'a'
+
+/*
+ * Задача 2
+ * Напиши функцию, которая проверяет,
+ * являются ли две строки анаграммами.
+ *
+ * Игнорировать пробелы и регистр.
+ */
+
+function isAnagram(str1, str2) {
+  const result1 = str1
+    .toLowerCase()
+    .replaceAll(' ', '')
+    .split('')
+    .sort()
+    .join('');
+
+  const result2 = str2
+    .toLowerCase()
+    .replaceAll(' ', '')
+    .split('')
+    .sort()
+    .join('');
+
+  return result1 === result2;
+}
+
+// console.log(isAnagram('listen', 'silent')); // true
+// console.log(isAnagram('Astronomer', 'Moon starer')); // true
+// console.log(isAnagram('hello', 'world')); // false
+// console.log(isAnagram('Debit Card', 'Bad Credit')); // true
